211bbf3297f7d96f3242af4740bbd56c1d0202c4
GFerreiroS <info@gferreiro.com>
2025-12-29 18:33:55 +0100

    config: removed user and pasword

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,31 +2,14 @@ import fs from "fs";
 import yaml from "js-yaml";
 import { z } from "zod";
 
-const ConfigSchema = z.object({
+export const ConfigSchema = z.object({
   dashboard: z.object({
     name: z.string(),
     refresh_seconds: z.number().int().positive(),
   }),
   server: z.object({
     base_url: z.string().url(),
-    username: z.string(),
-    password: z.string(),
   }),
-  map: z
-    .object({
-      image_path: z.string(),
-      image: z.object({
-        width: z.number().int().positive(),
-        height: z.number().int().positive(),
-      }),
-      world: z.object({
-        min_x: z.number(),
-        max_x: z.number(),
-        min_y: z.number(),
-        max_y: z.number(),
-      }),
-    })
-    .optional(),
 });
 
 export type AppConfig = z.infer<typeof ConfigSchema>;
